f81ea45633ad773374e0d0052e8607fed0d3a217
Ross Nordby <norbo@bepuentertainment.com>
2017-02-28 00:56:57 -0600

    Key value variant. (note both are still slower than built in array.sorts)

--- SolverPrototype/SolverPrototype/Quicksort.cs
+++ SolverPrototype/SolverPrototype/Quicksort.cs
@@ -16,6 +16,7 @@ namespace SolverPrototype
             a = b;
             b = temp;
         }
+
         public static void MappedSort<T, TComparer>(ref T keys, ref int indexMap, int l, int r, ref TComparer comparer) where TComparer : IComparerRef<T>
         {
             if (r - l <= 30)
@@ -25,18 +26,18 @@ namespace SolverPrototype
                 {
                     var originalIndexMap = Unsafe.Add(ref indexMap, i);
                     ref var original = ref Unsafe.Add(ref keys, originalIndexMap);
-                    var targetIndex = i;
-                    for (int compareIndex = i - 1; compareIndex >= l; --compareIndex)
+                    int compareIndex;
+                    for (compareIndex = i - 1; compareIndex >= l; --compareIndex)
                     {
                         if (comparer.Compare(ref original, ref Unsafe.Add(ref keys, Unsafe.Add(ref indexMap, compareIndex))) < 0)
                         {
                             //Move the index up.
                             Unsafe.Add(ref indexMap, compareIndex + 1) = Unsafe.Add(ref indexMap, compareIndex);
-                            targetIndex = compareIndex;
                         }
                         else
                             break;
                     }
+                    var targetIndex = compareIndex + 1;
                     if (targetIndex != i)
                     {
                         //Move the original index down.
@@ -158,6 +159,163 @@ namespace SolverPrototype
                 }
                 MappedSort(ref keys, ref indexMap, l, j, ref comparer);
                 MappedSort(ref keys, ref indexMap, i, r, ref comparer);
+            }
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void Swap<TKey, TValues>(ref TKey keys, ref TValues values, int a, int b)
+        {
+            Swap(ref Unsafe.Add(ref keys, a), ref Unsafe.Add(ref keys, b));
+            Swap(ref Unsafe.Add(ref values, a), ref Unsafe.Add(ref values, b));
+        }
+
+
+        public static void Sort<TKey, TValue, TComparer>(ref TKey keys, ref TValue values, int l, int r, ref TComparer comparer) where TComparer : IComparerRef<TKey>
+        {
+            if (r - l <= 30)
+            {
+                //The area to address is very small. Use insertion sort.
+                for (int i = l + 1; i <= r; ++i)
+                {
+                    var originalKey = Unsafe.Add(ref keys, i);
+                    var originalValue = Unsafe.Add(ref values, i);
+                    int compareIndex;
+                    for (compareIndex = i - 1; compareIndex >= l; --compareIndex)
+                    {
+                        if (comparer.Compare(ref originalKey, ref Unsafe.Add(ref keys, compareIndex)) < 0)
+                        {
+                            //Move the element up one slot.
+                            var upperSlotIndex = compareIndex + 1;
+                            Unsafe.Add(ref keys, upperSlotIndex) = Unsafe.Add(ref keys, compareIndex);
+                            Unsafe.Add(ref values, upperSlotIndex) = Unsafe.Add(ref values, compareIndex);
+                        }
+                        else
+                            break;
+                    }
+                    var targetIndex = compareIndex + 1;
+                    if (targetIndex != i)
+                    {
+                        //Move the original index down.
+                        Unsafe.Add(ref keys, targetIndex) = originalKey;
+                        Unsafe.Add(ref values, targetIndex) = originalValue;
+                    }
+                }
+            }
+            else
+            {
+                //Use MO3 to find a pivot to compensate for the common already-sorted case and to slightly improve worst-case behavior.
+                var first = Unsafe.Add(ref keys, l);
+                int middleIndex = (l + r) / 2;
+                var middle = Unsafe.Add(ref keys, middleIndex);
+                var last = Unsafe.Add(ref keys, r);
+                int pivotIndex;
+                TKey pivot;
+                if (comparer.Compare(ref first, ref middle) <= 0 && comparer.Compare(ref first, ref last) <= 0)
+                {
+                    //first is lowest.
+                    if (comparer.Compare(ref middle, ref last) <= 0)
+                    {
+                        pivotIndex = middleIndex;
+                        pivot = middle;
+                    }
+                    else
+                    {
+                        pivotIndex = r;
+                        pivot = last;
+                    }
+                }
+                else if (comparer.Compare(ref middle, ref last) <= 0)
+                {
+                    //middle is lowest.
+                    if (comparer.Compare(ref first, ref last) <= 0)
+                    {
+                        pivotIndex = l;
+                        pivot = first;
+                    }
+                    else
+                    {
+                        pivotIndex = r;
+                        pivot = last;
+                    }
+                }
+                else
+                {
+                    //last is lowest.
+                    if (comparer.Compare(ref first, ref middle) <= 0)
+                    {
+                        pivotIndex = l;
+                        pivot = first;
+                    }
+                    else
+                    {
+                        pivotIndex = middleIndex;
+                        pivot = middle;
+                    }
+                }
+
+                //Put the pivot into the last slot.
+                Swap(ref keys, ref values, pivotIndex, r);
+
+                //Use bentley-mcilroy 3-way partitioning scheme to avoid performance drops in corner cases.
+                int i = l - 1; //Start one below the partitioning area, because don't know if the first index is actually claimable.
+                int j = r; //The last element of the partition holds the pivot, which is excluded from the swapping process. Same logic as for i.
+                int p = l - 1;
+                int q = r;
+                if (r <= l)
+                    return;
+                while (true)
+                {
+                    //Claim the chunk of the list which is partitioned on the left and right sides.
+                    while (true)
+                    {
+                        ++i;
+                        if (comparer.Compare(ref Unsafe.Add(ref keys, i), ref pivot) >= 0)
+                            break;
+                    }
+                    while (true)
+                    {
+                        --j;
+                        if (comparer.Compare(ref pivot, ref Unsafe.Add(ref keys, j)) >= 0 || j == l)
+                            break;
+
+                    }
+                    //If the claims have met, then the partition is complete.
+                    if (i >= j)
+                    {
+                        break;
+                    }
+                    //By the claiming above and because we did not yet break out of the loop,
+                    //the value associated with i is >= the pivot, and the value associated with j is <= the pivot.
+                    //So swap them.
+                    Swap(ref keys, ref values, i, j);
+                    if (comparer.Compare(ref Unsafe.Add(ref keys, i), ref pivot) == 0)
+                    {
+                        p++;
+                        Swap(ref keys, ref values, p, i);
+                    }
+                    if (comparer.Compare(ref pivot, ref Unsafe.Add(ref keys, j)) == 0)
+                    {
+                        q--;
+                        Swap(ref keys, ref values, j, q);
+                    }
+                }
+                //The pivot at r has not been swapped.
+                //Since the loop has terminated, we know that i has reached the the '>=pivot' side of the partition.
+                //So, swap the pivot and the first element of the greater-than-pivot side to guarantee sorting.
+                Swap(ref keys, ref values, i, r);
+                j = i - 1;
+                i = i + 1;
+                for (int k = l; k < p; k++, j--)
+                {
+                    Swap(ref keys, ref values, k, j);
+                }
+                for (int k = r - 1; k > q; k--, i++)
+                {
+                    Swap(ref keys, ref values, i, k);
+                }
+                Sort(ref keys, ref values, l, j, ref comparer);
+                Sort(ref keys, ref values, i, r, ref comparer);
             }
         }
     }
--- SolverPrototype/SolverPrototypeTests/SortTest.cs
+++ SolverPrototype/SolverPrototypeTests/SortTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace SolverPrototypeTests
     {
         struct Comparer : IComparerRef<int>
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public int Compare(ref int a, ref int b)
             {
                 return a < b ? -1 : a > b ? 1 : 0;
@@ -29,26 +31,27 @@ namespace SolverPrototypeTests
                 indexMap[i] = i;
                 keys[i] = random.Next();
             }
+            int[] keys2 = new int[elementCount];
             var indexMap2 = new int[elementCount];
             Array.Copy(indexMap, indexMap2, elementCount);
+            Array.Copy(keys, keys2, elementCount);
 
             var comparer = new Comparer();
-            Quicksort.MappedSort(ref keys[0], ref indexMap[0], 0, 0, ref comparer); //prejit
+            Quicksort.Sort(ref keys[0], ref indexMap[0], 0, 0, ref comparer); //prejit
             var timer = Stopwatch.StartNew();
-            Quicksort.MappedSort(ref keys[0], ref indexMap[0], 0, elementCount - 1, ref comparer);
-
+            Quicksort.Sort(ref keys[0], ref indexMap[0], 0, elementCount - 1, ref comparer);
             timer.Stop();
             Console.WriteLine($"QSort time (ms): {timer.Elapsed.TotalSeconds * 1e3}");
-            int[] result = new int[elementCount];
+            int[] mappedResult = new int[elementCount];
             for (int i = 0; i < elementCount; ++i)
             {
-                result[i] = keys[indexMap[i]];
+                mappedResult[i] = keys[indexMap[i]];
             }
 
 
-            Array.Sort(keys, indexMap2, 0, 1); //prejit
+            Array.Sort(keys2, indexMap2, 0, 1); //prejit
             timer.Restart();
-            Array.Sort(keys, indexMap2, 0, elementCount);
+            Array.Sort(keys2, indexMap2, 0, elementCount);
             timer.Stop();
             Console.WriteLine($"Array.Sort time (ms): {timer.Elapsed.TotalSeconds * 1e3}");
         }
