ea11b5816a497cba3982610512abfed503add759
Ross Nordby <norbo@bepuentertainment.com>
2017-02-28 19:05:11 -0600

    Simple scalar radix sort added.

--- SolverPrototype/SolverPrototypeTests/SortTest.cs
+++ SolverPrototype/SolverPrototypeTests/SortTest.cs
@@ -31,10 +31,15 @@ namespace SolverPrototypeTests
                 indexMap[i] = i;
                 keys[i] = random.Next();
             }
-            int[] keys2 = new int[elementCount];
+            var keys2 = new int[elementCount];
             var indexMap2 = new int[elementCount];
+            var keys3 = new int[elementCount];
+            var indexMap3 = new int[elementCount];
             Array.Copy(indexMap, indexMap2, elementCount);
             Array.Copy(keys, keys2, elementCount);
+            Array.Copy(indexMap, indexMap3, elementCount);
+            Array.Copy(keys, keys3, elementCount);
+
 
             var comparer = new Comparer();
             Quicksort.Sort(ref keys[0], ref indexMap[0], 0, 0, ref comparer); //prejit
@@ -47,6 +52,18 @@ namespace SolverPrototypeTests
             {
                 mappedResult[i] = keys[indexMap[i]];
             }
+
+
+            var keysScratch = new int[elementCount];
+            var valuesScratch = new int[elementCount];
+            var bucketCounts = new int[1024];
+            Array.Clear(bucketCounts, 0, bucketCounts.Length);
+            RadixSort.SortU32(ref keys3[0], ref indexMap3[0], ref keysScratch[0], ref valuesScratch[0], ref bucketCounts[0], 1); //prejit
+            timer.Restart();
+            Array.Clear(bucketCounts, 0, bucketCounts.Length);
+            RadixSort.SortU32(ref keys3[0], ref indexMap3[0], ref keysScratch[0], ref valuesScratch[0], ref bucketCounts[0], elementCount); //prejit
+            timer.Stop();
+            Console.WriteLine($"RadixSort time (ms): {timer.Elapsed.TotalSeconds * 1e3}");
 
 
             Array.Sort(keys2, indexMap2, 0, 1); //prejit
