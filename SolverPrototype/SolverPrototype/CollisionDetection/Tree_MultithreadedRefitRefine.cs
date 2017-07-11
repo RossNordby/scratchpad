de0d96ba8bbfb71f953879adde8f174d814e35a5
Ross Nordby <norbo@bepuentertainment.com>
2017-07-11 15:36:56 -0500

    Further cleaned up refit refine framework.

--- SolverPrototype/SolverPrototype/CollisionDetection/Tree_MultithreadedRefitRefine.cs
+++ SolverPrototype/SolverPrototype/CollisionDetection/Tree_MultithreadedRefitRefine.cs
@@ -17,7 +17,7 @@ namespace SolverPrototype.CollisionDetection
         public class RefitAndRefineMultithreadedContext
         {
             Tree Tree;
-            
+
             int RefitNodeIndex;
             QuickList<int, Buffer<int>> RefitNodes;
             float RefitCostChange;
@@ -44,7 +44,8 @@ namespace SolverPrototype.CollisionDetection
                 CacheOptimizeAction = CacheOptimize;
             }
 
-            public void RefitAndRefine(Tree tree, IThreadDispatcher threadDispatcher)
+            public unsafe void RefitAndRefine(Tree tree, IThreadDispatcher threadDispatcher, int frameIndex,
+                float refineAggressivenessScale = 1, float cacheOptimizeAggressivenessScale = 1)
             {
                 if (tree.leafCount <= 2)
                 {
@@ -59,6 +60,8 @@ namespace SolverPrototype.CollisionDetection
                 //we don't want to spend the time doing sync work. The candidates are then pruned down to a target single target set for the refine pass.
                 Tree.Pool.SpecializeFor<QuickList<int, Buffer<int>>>().Take(threadDispatcher.ThreadCount, out RefinementCandidates);
                 tree.GetRefitAndMarkTuning(out MaximumSubtrees, out var LeafCountThreshold, out var refinementLeafCountThreshold);
+                //Note that the number of refit nodes is not necessarily bound by MaximumSubtrees. It is just a heuristic estimate. Resizing has to be supported.
+                QuickList<int, Buffer<int>>.Create(tree.Pool.SpecializeFor<int>(), MaximumSubtrees, out RefitNodes);
                 //Note that we haven't rigorously guaranteed a refinement count maximum, so it's possible that the other threads will need to resize the per-thread refinement candidate lists.
                 //So we don't create the refinement candidate thread lists here; we let the worker do it.
 
@@ -66,12 +69,86 @@ namespace SolverPrototype.CollisionDetection
                 int multithreadingLeafCountThreshold = Tree.leafCount / (threadDispatcher.ThreadCount * 2);
                 if (multithreadingLeafCountThreshold < refinementLeafCountThreshold)
                     multithreadingLeafCountThreshold = refinementLeafCountThreshold;
-                CollectNodesForMultithreadedRefit(0, multithreadingLeafCountThreshold, ref RefitNodes, refinementLeafCountThreshold, ref RefinementCandidates[0], 
+                CollectNodesForMultithreadedRefit(0, multithreadingLeafCountThreshold, ref RefitNodes, refinementLeafCountThreshold, ref RefinementCandidates[0],
                     threadDispatcher.GetThreadMemoryPool(0).SpecializeFor<int>());
 
                 threadDispatcher.DispatchWorkers(RefitAndMarkAction);
 
+                //Condense the set of candidates into a set of targets.
+                int refinementCandidatesCount = 0;
+                for (int i = 0; i < threadDispatcher.ThreadCount; ++i)
+                {
+                    refinementCandidatesCount += RefinementCandidates[i].Count;
 
+                }
+                Tree.GetRefineTuning(frameIndex, refinementCandidatesCount, refineAggressivenessScale, RefitCostChange, threadDispatcher.ThreadCount,
+                    out var targetRefinementCount, out var period, out var offset);
+                QuickList<int, Buffer<int>>.Create(tree.Pool.SpecializeFor<int>(), targetRefinementCount, out RefinementTargets);
+
+                //Note that only a subset of all refinement *candidates* will become refinement *targets*.
+                //We start at a semirandom offset and then skip through the set to accumulate targets.
+                //The number of candidates that become targets is based on the refinement aggressiveness,
+                //tuned by both user input (the scale) and on the volatility of the tree (RefitCostChange).
+                var currentCandidatesIndex = 0;
+                int index = offset;
+                for (int i = 0; i < targetRefinementCount - 1; ++i)
+                {
+                    index += period;
+                    //Wrap around if the index doesn't fit.
+                    while (index >= RefinementCandidates[currentCandidatesIndex].Count)
+                    {
+                        index -= RefinementCandidates[currentCandidatesIndex].Count;
+                        ++currentCandidatesIndex;
+                        if (currentCandidatesIndex >= threadDispatcher.ThreadCount)
+                            currentCandidatesIndex -= threadDispatcher.ThreadCount;
+                    }
+                    Debug.Assert(index < RefinementCandidates[currentCandidatesIndex].Count && index >= 0);
+                    var nodeIndex = RefinementCandidates[currentCandidatesIndex][index];
+                    RefinementTargets.AddUnsafely(nodeIndex);
+                    tree.nodes[nodeIndex].RefineFlag = 1;
+                }
+                //Note that the root node is only refined if it was not picked as a target earlier.
+                if (tree.nodes->RefineFlag != 1)
+                {
+                    RefinementTargets.AddUnsafely(0);
+                    tree.nodes->RefineFlag = 1;
+                }
+
+
+                threadDispatcher.DispatchWorkers(RefineAction);
+
+                //To multithread this, give each worker a contiguous chunk of nodes. You want to do the biggest chunks possible to chain decent cache behavior as far as possible.
+                //Note that more cache optimization is required with more threads, since spreading it out more slightly lessens its effectiveness.
+                var cacheOptimizeCount = Tree.GetCacheOptimizeTuning(MaximumSubtrees, RefitCostChange, (Math.Max(1, threadDispatcher.ThreadCount * 0.25f)) * cacheOptimizeAggressivenessScale);
+
+                var cacheOptimizationTasks = threadDispatcher.ThreadCount * 2;
+                PerWorkerCacheOptimizeCount = cacheOptimizeCount / cacheOptimizationTasks;
+                var startIndex = (int)(((long)frameIndex * PerWorkerCacheOptimizeCount) % Tree.nodeCount);
+                QuickList<int, Buffer<int>>.Create(Tree.Pool.SpecializeFor<int>(), cacheOptimizationTasks, out CacheOptimizeStarts);
+                CacheOptimizeStarts.AddUnsafely(startIndex);
+
+                var optimizationSpacing = Tree.nodeCount / threadDispatcher.ThreadCount;
+                var optimizationSpacingWithExtra = optimizationSpacing + 1;
+                var optimizationRemainder = Tree.nodeCount - optimizationSpacing * threadDispatcher.ThreadCount;
+
+                for (int i = 1; i < cacheOptimizationTasks; ++i)
+                {
+                    if (optimizationRemainder > 0)
+                    {
+                        startIndex += optimizationSpacingWithExtra;
+                        --optimizationRemainder;
+                    }
+                    else
+                    {
+                        startIndex += optimizationSpacing;
+                    }
+                    if (startIndex >= Tree.nodeCount)
+                        startIndex -= Tree.nodeCount;
+                    Debug.Assert(startIndex >= 0 && startIndex < Tree.nodeCount);
+                    CacheOptimizeStarts.AddUnsafely(startIndex);
+                }
+
+                threadDispatcher.DispatchWorkers(CacheOptimizeAction);
 
                 RefitNodeIndex = -1;
                 RefineIndex = -1;
@@ -81,13 +158,16 @@ namespace SolverPrototype.CollisionDetection
                     RefinementCandidates[i].Dispose(threadDispatcher.GetThreadMemoryPool(i).SpecializeFor<int>());
                 }
                 Tree.Pool.SpecializeFor<QuickList<int, Buffer<int>>>().Return(ref RefinementCandidates);
+                RefitNodes.Dispose(Tree.Pool.SpecializeFor<int>());
+                RefinementTargets.Dispose(Tree.Pool.SpecializeFor<int>());
+                CacheOptimizeStarts.Dispose(Tree.Pool.SpecializeFor<int>());
                 Tree = null;
                 this.threadDispatcher = null;
             }
 
             unsafe void CollectNodesForMultithreadedRefit(int nodeIndex,
                 int multithreadingLeafCountThreshold, ref QuickList<int, Buffer<int>> refitAndMarkTargets,
-                int refinementLeafCountThreshold, ref QuickList<int, Buffer<int>> refinementCandidates, BufferPool<int> intPool)
+                int refinementLeafCountThreshold, ref QuickList<int, Buffer<int>> refinementCandidates, BufferPool<int> threadIntPool)
             {
                 var node = Tree.nodes + nodeIndex;
                 var children = &node->A;
@@ -105,18 +185,18 @@ namespace SolverPrototype.CollisionDetection
                             if (child.LeafCount <= refinementLeafCountThreshold)
                             {
                                 //It's possible that a wavefront node is this high in the tree, so it has to be captured here because the postpass won't find it.
-                                refinementCandidates.Add(child.Index, intPool);
+                                refinementCandidates.Add(child.Index, threadIntPool);
                                 //Encoding the child index tells the thread to use RefitAndMeasure instead of RefitAndMark since this was a wavefront node.
-                                refitAndMarkTargets.Add(Encode(child.Index), intPool);
+                                refitAndMarkTargets.Add(Encode(child.Index), Tree.Pool.SpecializeFor<int>());
                             }
                             else
                             {
-                                refitAndMarkTargets.Add(child.Index, intPool);
+                                refitAndMarkTargets.Add(child.Index, Tree.Pool.SpecializeFor<int>());
                             }
                         }
                         else
                         {
-                            CollectNodesForMultithreadedRefit(child.Index, multithreadingLeafCountThreshold, ref refitAndMarkTargets, refinementLeafCountThreshold, ref refinementCandidates, intPool);
+                            CollectNodesForMultithreadedRefit(child.Index, multithreadingLeafCountThreshold, ref refitAndMarkTargets, refinementLeafCountThreshold, ref refinementCandidates, threadIntPool);
                         }
                     }
                 }
@@ -247,7 +327,7 @@ namespace SolverPrototype.CollisionDetection
                 var subtreeCountEstimate = 1 << SpanHelper.GetContainingPowerOf2(MaximumSubtrees);
                 QuickList<int, Buffer<int>>.Create(threadIntPool, subtreeCountEstimate, out var subtreeReferences);
                 QuickList<int, Buffer<int>>.Create(threadIntPool, subtreeCountEstimate, out var treeletInternalNodes);
-                
+
                 int refineIndex;
                 while ((refineIndex = Interlocked.Increment(ref RefineIndex)) < RefinementTargets.Count)
                 {
@@ -257,7 +337,7 @@ namespace SolverPrototype.CollisionDetection
                     //Allow other refines to traverse this node.
                     Tree.nodes[RefinementTargets[refineIndex]].RefineFlag = 0;
                 }
-                
+
                 subtreeReferences.Dispose(threadIntPool);
                 treeletInternalNodes.Dispose(threadIntPool);
 
@@ -301,142 +381,6 @@ namespace SolverPrototype.CollisionDetection
 
             }
         }
-
-        public unsafe int RefitAndRefine(int frameIndex, IParallelLooper looper, RefitAndRefineMultithreadedContext context, float refineAggressivenessScale = 1, float cacheOptimizeAggressivenessScale = 1)
-        {
-            //Don't proceed if the tree is empty.
-            if (leafCount == 0)
-                return 0;
-            var pool = BufferPools<int>.Locking;
-
-            int estimatedRefinementTargetCount;
-            GetRefitAndMarkTuning(out context.MaximumSubtrees, out estimatedRefinementTargetCount, out context.LeafCountThreshold);
-
-            context.Initialize(looper.ThreadCount, estimatedRefinementTargetCount, pool);
-
-            //Collect the refinement candidates.
-            if (LeafCount <= 2)
-            {
-                RefitAndMark(context.LeafCountThreshold, ref context.RefinementCandidates.Elements[0]);
-            }
-            else
-            {
-                CollectNodesForMultithreadedRefit(looper.ThreadCount, ref context.RefitNodes, context.LeafCountThreshold, ref context.RefinementCandidates.Elements[0]);
-                //Console.WriteLine($"Refit subtree count: {context.RefitNodes.Count}");
-                looper.ForLoop(0, looper.ThreadCount, context.RefitAndMarkAction);
-            }
-
-            var refinementCandidatesCount = 0;
-            for (int i = 0; i < looper.ThreadCount; ++i)
-            {
-                refinementCandidatesCount += context.RefinementCandidates.Elements[i].Count;
-            }
-
-            int targetRefinementCount, period, offset;
-            GetRefineTuning(frameIndex, refinementCandidatesCount, refineAggressivenessScale, context.RefitCostChange, looper.ThreadCount, out targetRefinementCount, out period, out offset);
-
-
-
-            //Condense the set of candidates into a set of targets.
-            context.RefinementTargets = new QuickList<int>(pool, BufferPool<int>.GetPoolIndex(targetRefinementCount));
-
-
-            int actualRefinementTargetsCount = 0;
-            var currentCandidatesIndex = 0;
-            int index = offset;
-            for (int i = 0; i < targetRefinementCount - 1; ++i)
-            {
-                index += period;
-                //Wrap around if the index doesn't fit.
-                while (index >= context.RefinementCandidates.Elements[currentCandidatesIndex].Count)
-                {
-                    index -= context.RefinementCandidates.Elements[currentCandidatesIndex].Count;
-                    ++currentCandidatesIndex;
-                    if (currentCandidatesIndex >= context.RefinementCandidates.Count)
-                        currentCandidatesIndex -= context.RefinementCandidates.Count;
-                }
-                Debug.Assert(index < context.RefinementCandidates.Elements[currentCandidatesIndex].Count && index >= 0);
-                var nodeIndex = context.RefinementCandidates.Elements[currentCandidatesIndex].Elements[index];
-                context.RefinementTargets.Elements[actualRefinementTargetsCount++] = nodeIndex;
-                nodes[nodeIndex].RefineFlag = 1;
-            }
-            context.RefinementTargets.Count = actualRefinementTargetsCount;
-            if (nodes->RefineFlag != 1)
-            {
-                context.RefinementTargets.Add(0);
-                ++actualRefinementTargetsCount;
-                nodes->RefineFlag = 1;
-            }
-
-
-
-            //Refine all marked targets.
-            looper.ForLoop(0, Math.Min(looper.ThreadCount, context.RefinementTargets.Count), context.RefineAction);
-
-
-            //There's no reason to perform any kind of cache optimization on a tree with less than 2 leaf nodes.
-            //That, combined with using a binary tree, gives us a guarantee: every node has 2 children. No need for per-node child counts.
-            if (leafCount > 2)
-            {
-                //To multithread this, give each worker a contiguous chunk of nodes. You want to do the biggest chunks possible to chain decent cache behavior as far as possible.
-                //Note that more cache optimization is required with more threads, since spreading it out more slightly lessens its effectiveness.
-                var cacheOptimizeCount = GetCacheOptimizeTuning(context.MaximumSubtrees, context.RefitCostChange, (Math.Max(1, looper.ThreadCount * 0.25f)) * cacheOptimizeAggressivenessScale);
-
-                var cacheOptimizationTasks = looper.ThreadCount * 2;
-                context.PerWorkerCacheOptimizeCount = cacheOptimizeCount / cacheOptimizationTasks;
-                var startIndex = (int)(((long)frameIndex * context.PerWorkerCacheOptimizeCount) % nodeCount);
-                context.CacheOptimizeStarts.Add(startIndex);
-
-                var optimizationSpacing = nodeCount / looper.ThreadCount;
-                var optimizationSpacingWithExtra = optimizationSpacing + 1;
-                var optimizationRemainder = nodeCount - optimizationSpacing * looper.ThreadCount;
-
-                for (int i = 1; i < cacheOptimizationTasks; ++i)
-                {
-                    if (optimizationRemainder > 0)
-                    {
-                        startIndex += optimizationSpacingWithExtra;
-                        --optimizationRemainder;
-                    }
-                    else
-                    {
-                        startIndex += optimizationSpacing;
-                    }
-                    if (startIndex >= nodeCount)
-                        startIndex -= nodeCount;
-                    Debug.Assert(startIndex >= 0 && startIndex < nodeCount);
-                    context.CacheOptimizeStarts.Add(startIndex);
-                }
-
-                //for (int i = 0; i < looper.ThreadCount; ++i)
-                //{
-                //    var start = context.CacheOptimizeStarts[i];
-                //    var end = Math.Min(start + context.PerWorkerCacheOptimizeCount, NodeCount);
-                //    for (int j = start; j < end; ++j)
-                //    {
-                //        //ValidateRefineFlags(0);
-                //        IncrementalCacheOptimizeThreadSafe(j);
-                //        //ValidateRefineFlags(0);
-                //    }
-                //}
-
-
-                //var start = Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency;
-                //Validate();
-                //ValidateRefineFlags(0);
-                looper.ForLoop(0, cacheOptimizationTasks, context.CacheOptimizeAction);
-                //ValidateRefineFlags(0);
-                //var end = Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency;
-
-                //Validate();
-                //Console.WriteLine($"Cache optimize time: {end - start}");
-            }
-
-
-            context.CleanUp();
-            return actualRefinementTargetsCount;
-        }
-
-
+        
     }
 }
