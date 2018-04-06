e3e633decfc3b2705666d2544b19e152ee4c430c
Ross Nordby <norbo@bepuentertainment.com>
2018-04-06 13:34:04 -0500

    Added a SIMD width checker utility.

---
+++ SIMDWidthChecker/SIMDWidthChecker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Numerics;
+
+namespace SIMDWidthChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SIMD Width: {Vector<float>.Count}");
+            Console.ReadKey();
+        }
+    }
+}
