ee81ed7693981a6871a67cf81afedf5491d211e3
Ross Nordby <norbo@bepuentertainment.com>
2020-07-02 18:44:25 -0500

    Another packing commit.

--- AutomagicWorkflows/SomeTests/UnitTest1.cs
+++ AutomagicWorkflows/SomeTests/UnitTest1.cs
@@ -1,15 +1,29 @@
 using SomeLibrary;
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace SomeTests
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void Test1()
         {
             Assert.True(Tarben.Emulsify(5) == 15, "ya dingus");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            output.WriteLine($"count: {Environment.ProcessorCount}");
+            Assert.True(Environment.ProcessorCount > 1);
         }
     }
 }
