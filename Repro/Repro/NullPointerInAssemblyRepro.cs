fe8aeea5232b49632c040730ee200ba6a0994113
Ross Nordby <norbo@bepuentertainment.com>
2020-07-15 19:29:16 -0500

    5.0 null pointer asm repro.

---
+++ Repro/Repro/NullPointerInAssemblyRepro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Repro
+{
+    public static class NullPointerInAssemblyRepro
+    {
+        struct Container
+        {
+            public Vector<int> Vector;
+            public int Integer;
+        }
+
+        static Vector<int> DoAThingByRef(ref Vector<int> s)
+        {
+            return s + Vector<int>.Zero;
+        }
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static unsafe Vector<int> Test()
+        {
+            Container container = default;
+            return DoAThingByRef(ref container.Vector);
+        }
+
+        public static void TestRepro()
+        {
+            Test();
+        }
+    }
+}
--- Repro/Repro/Program.cs
+++ Repro/Repro/Program.cs
@@ -8,6 +8,7 @@ namespace Repro
         {
             ExecutionEngineExceptionRepro.Test();
             BoundingBoxRepro.Test();
+            NullPointerInAssemblyRepro.TestRepro();
         }
     }
 }
