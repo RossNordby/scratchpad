c2123165423297b457cd5afee1d7bb4694a2c884
Ross Nordby <norbo@bepuentertainment.com>
2022-02-07 20:02:58 -0600

    More demos, more infrastructure.

--- HeadlessTests24/HeadlessTests24/Program.cs
+++ HeadlessTests24/HeadlessTests24/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Numerics;
+﻿using HeadlessTests24.DemoStyle;
+using HeadlessTests24.DemoStyle.Dancers;
+using System.Numerics;
+using System.Text;
 
 List<int> threadCounts = new List<int>();
 const string threadCountsPath = "threadCounts.txt";
@@ -26,11 +29,11 @@ catch
         threadCounts.Add(threadCount);
         threadCount >>= 1;
     }
-    if (threadCount >= 4)
+    if (Environment.ProcessorCount >= 4)
     {
         threadCounts.Add(Environment.ProcessorCount * 3 / 4);
     }
-    if (threadCount >= 8)
+    if (Environment.ProcessorCount >= 8)
     {
         threadCounts.Add(Environment.ProcessorCount * 3 / 8);
     }
@@ -41,3 +44,38 @@ for (int i = 0; i < threadCounts.Count; ++i)
     Console.Write(i < threadCounts.Count - 1 ? $"{threadCounts[i]}, " : threadCounts[i]);
 }
 Console.WriteLine();
+
+var builder = new StringBuilder(2048);
+
+
+
+builder.Append("Demo");
+for (int i = 0; i < threadCounts.Count; ++i)
+{
+    builder.Append(" ").Append(threadCounts[i]);
+}
+builder.AppendLine();
+void ExecuteDemoStyle<T>(int runCount, int preframeCount, int frameCount) where T : Demo, new()
+{
+    string name = typeof(T).Name;
+    builder.Append(name).Append(" ");
+    for (int i = 0; i < threadCounts.Count; ++i)
+    {
+        Console.WriteLine($"@@ Testing {name} with {threadCounts[i]} threads. @@");
+        var times = new List<double>();
+        DemoHeadlessTest.Test<ColosseumDemo>(threadCounts[i], runCount, preframeCount, frameCount, times);
+        //Just write the minimum for now.
+        double minimum = times[0];
+        for (int j = 1; j < times.Count; ++j)
+        {
+            minimum = Math.Min(minimum, times[j]);
+        }
+        builder.Append(minimum);
+    }
+    builder.AppendLine();
+}
+
+ExecuteDemoStyle<ColosseumDemo>(2, 32, 256);
+ExecuteDemoStyle<DancerDemo>(2, 32, 256);
+
+Console.WriteLine(builder.ToString());
