17d07c030f04fb45650f50e76d51a51dac8e63e9
Ross Nordby <norbo@bepuentertainment.com>
2022-03-14 21:27:55 -0500

    No runtime/bootstrap references.

---
+++ Abomination/AbominationInterop/AbominationInterop/Entrypoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbominationInterop
+{
+    internal static class Entrypoints
+    {
+        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = "Goingtr")]
+        public static void Greetings()
+        {
+            Console.WriteLine("Proing");
+        }
+    }
+}
---
+++ Abomination/AbominationInterop/Native/NativeCMake/NativeCMake.cpp
@@ -0,0 +1,12 @@
+﻿// NativeCMake.cpp : Defines the entry point for the application.
+//
+
+#include "NativeCMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
