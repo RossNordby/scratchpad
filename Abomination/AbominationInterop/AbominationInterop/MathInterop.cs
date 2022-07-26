41f04933dbf08df858c60048ebeb451da14dcb0d
Ross Nordby <norbo@bepuentertainment.com>
2022-07-26 17:20:55 -0500

    Refactoring.

---
+++ Abomination/AbominationInterop/AbominationInterop/MathInterop.cs
@@ -0,0 +1,72 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics;
+
+namespace AbominationInterop;
+
+/// <summary>
+/// Vector3Wide interop type used when <see cref="Vector{float}"/> is 128 bits wide.
+/// </summary>
+public struct Vector3SIMD128
+{
+    public Vector128<float> X;
+    public Vector128<float> Y;
+    public Vector128<float> Z;
+}
+
+/// <summary>
+/// Vector3Wide interop type used when <see cref="Vector{float}"/> is 256 bits wide.
+/// </summary>
+public struct Vector3SIMD256
+{
+    public Vector256<float> X;
+    public Vector256<float> Y;
+    public Vector256<float> Z;
+}
+
+/// <summary>
+/// QuaternionWide interop type used when <see cref="Vector{float}"/> is 128 bits wide.
+/// </summary>
+public struct QuaternionSIMD128
+{
+    public Vector128<float> X;
+    public Vector128<float> Y;
+    public Vector128<float> Z;
+}
+
+/// <summary>
+/// QuaternionWide interop type used when <see cref="Vector{float}"/> is 256 bits wide.
+/// </summary>
+public struct QuaternionSIMD256
+{
+    public Vector256<float> X;
+    public Vector256<float> Y;
+    public Vector256<float> Z;
+}
+
+/// <summary>
+/// BodyInertiaWide interop type used when <see cref="Vector{float}"/> is 128 bits wide.
+/// </summary>
+public struct BodyInertiaSIMD128
+{
+    public Vector128<float> InverseInertiaXX;
+    public Vector128<float> InverseInertiaYX;
+    public Vector128<float> InverseInertiaYY;
+    public Vector128<float> InverseInertiaZX;
+    public Vector128<float> InverseInertiaZY;
+    public Vector128<float> InverseInertiaZZ;
+    public Vector128<float> InverseMass;
+}
+
+/// <summary>
+/// BodyInertiaWide interop type used when <see cref="Vector{float}"/> is 256 bits wide.
+/// </summary>
+public struct BodyInertiaSIMD256
+{
+    public Vector256<float> InverseInertiaXX;
+    public Vector256<float> InverseInertiaYX;
+    public Vector256<float> InverseInertiaYY;
+    public Vector256<float> InverseInertiaZX;
+    public Vector256<float> InverseInertiaZY;
+    public Vector256<float> InverseInertiaZZ;
+    public Vector256<float> InverseMass;
+}
---
+++ Abomination/AbominationInterop/AbominationInterop/PoseIntegratorCallbacks.cs
@@ -0,0 +1,109 @@
+﻿using BepuPhysics;
+using BepuUtilities;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+
+namespace AbominationInterop;
+
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct PoseIntegratorCallbacksInterop
+{
+    [FieldOffset(0)]
+    public AngularIntegrationMode AngularIntegrationMode;
+    [FieldOffset(4)]
+    public byte AllowSubstepsForUnconstrainedBodies;
+    [FieldOffset(5)]
+    public byte IntegrateVelocityForKinematics;
+    [FieldOffset(6)]
+    public byte UseScalarCallback;
+
+    [FieldOffset(8)]
+    public delegate* unmanaged<InstanceHandle, void> Initialize;
+    [FieldOffset(16)]
+    public delegate* unmanaged<InstanceHandle, float, void> PrepareForIntegration;
+    //There is technically no need to expose all three of these in the interop type as separate fields; we may want to change that.
+    //Right now, we're doing it just so that the signature is more explicit... but that could be better handled on the native side.
+    [FieldOffset(24)]
+    public delegate* unmanaged<InstanceHandle, float, void> IntegrateVelocityScalar;
+    [FieldOffset(32)]
+    public delegate* unmanaged<InstanceHandle, Vector128<int>, Vector3SIMD128*, QuaternionSIMD128*, BodyInertiaSIMD128*, Vector128<int>, int, Vector128<float>, void> IntegrateVelocitySIMD128;
+    [FieldOffset(40)]
+    public delegate* unmanaged<InstanceHandle, Vector256<int>, Vector3SIMD256*, QuaternionSIMD256*, BodyInertiaSIMD256*, Vector256<int>, int, Vector256<float>, void> IntegrateVelocitySIMD256;
+}
+
+struct AngularIntegrationModeNonconserving { }
+struct AngularIntegrationModeConserve { }
+struct AngularIntegrationModeConserveWithGyroTorque { }
+struct True { }
+struct False { }
+
+//These value typed generic parameters will result in all the branching conditional logic in PoseIntegratorCallbacks getting elided.
+public unsafe struct PoseIntegratorCallbacks<TAngularConservationMode, TUnconstrainedSubstepping, TKinematicIntegration, TScalar> : IPoseIntegratorCallbacks
+{
+    public AngularIntegrationMode AngularIntegrationMode
+    {
+        get
+        {
+            if (typeof(TAngularConservationMode) == typeof(AngularIntegrationModeConserveWithGyroTorque))
+                return AngularIntegrationMode.ConserveMomentumWithGyroscopicTorque;
+            if (typeof(TAngularConservationMode) == typeof(AngularIntegrationModeConserve))
+                return AngularIntegrationMode.ConserveMomentum;
+            return AngularIntegrationMode.Nonconserving;
+        }
+    }
+
+    public bool AllowSubstepsForUnconstrainedBodies => typeof(TUnconstrainedSubstepping) == typeof(True);
+
+    public bool IntegrateVelocityForKinematics => typeof(TKinematicIntegration) == typeof(True);
+
+    public delegate* unmanaged<InstanceHandle, void> InitializeFunction;
+    public delegate* unmanaged<InstanceHandle, float, void> PrepareForIntegrationFunction;
+    public void* IntegrateVelocityFunction;
+    public InstanceHandle Simulation;
+
+    public void Initialize(Simulation simulation)
+    {
+        //No handle exists yet so we can't expose this to the native side.
+    }
+
+    public void Initialize(InstanceHandle simulation)
+    {
+        Simulation = simulation;
+        if (InitializeFunction != null)
+            InitializeFunction(simulation);
+    }
+
+    public void IntegrateVelocity(Vector<int> bodyIndices, Vector3Wide position, QuaternionWide orientation, BodyInertiaWide localInertia, Vector<int> integrationMask, int workerIndex, Vector<float> dt, ref BodyVelocityWide velocity)
+    {
+        if (typeof(TScalar) == typeof(True))
+        {
+            //Use the scalar codepath. Adds some overhead, but understandable that some users would want it. Working with the wide representation isn't always going to be obvious.
+            //TODO
+            var integrateVelocity = (delegate* unmanaged<InstanceHandle, float, void>)IntegrateVelocityFunction;
+        }
+        else
+        {
+            //Wide representation.
+            if (Vector<float>.Count == 4)
+            {
+                var integrateVelocity = (delegate* unmanaged<InstanceHandle, Vector128<int>, Vector3SIMD128*, QuaternionSIMD128*, BodyInertiaSIMD128*, Vector128<int>, int, Vector128<float>, void>)IntegrateVelocityFunction;
+                //TODO
+            }
+            else
+            {
+                Debug.Assert(Vector<float>.Count == 8, "For now we're assuming that SIMD vector width is always either 128 or 256.");
+                var integrateVelocity = (delegate* unmanaged<InstanceHandle, Vector256<int>, Vector3SIMD256*, QuaternionSIMD256*, BodyInertiaSIMD256*, Vector256<int>, int, Vector256<float>, void>)IntegrateVelocityFunction;
+                //TODO
+            }
+        }
+    }
+
+    public void PrepareForIntegration(float dt)
+    {
+        //Really SHOULD be a prepare function provided, but it's not technically required like the velocity integration one is.
+        if (PrepareForIntegrationFunction != null)
+            PrepareForIntegrationFunction(Simulation, dt);
+    }
+}
