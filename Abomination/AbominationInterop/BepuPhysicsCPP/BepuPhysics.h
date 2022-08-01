fe58db332a73dd039b8e350cd2d761ff3232d500
Ross Nordby <norbo@bepuentertainment.com>
2022-08-01 15:09:30 -0500

    Allocation sizes.

--- Abomination/AbominationInterop/BepuPhysicsCPP/BepuPhysics.h
+++ Abomination/AbominationInterop/BepuPhysicsCPP/BepuPhysics.h
@@ -1269,6 +1269,70 @@ namespace Bepu
 		}
 	};
 
+	/// <summary>
+	/// The common set of allocation sizes for a simulation.
+	/// </summary>
+	struct SimulationAllocationSizes
+	{
+		/// <summary>
+		/// The number of bodies to allocate space for.
+		/// </summary>
+		int32_t Bodies;
+		/// <summary>
+		/// The number of statics to allocate space for.
+		/// </summary>
+		int32_t Statics;
+		/// <summary>
+		/// The number of inactive islands to allocate space for.
+		/// </summary>
+		int32_t Islands;
+		/// <summary>
+		/// Minimum number of shapes to allocate space for in each shape type batch.
+		/// </summary>
+		int32_t ShapesPerType;
+		/// <summary>
+		/// The number of constraints to allocate bookkeeping space for. This does not affect actual type batch allocation sizes, only the solver-level constraint handle storage.
+		/// </summary>
+		int32_t Constraints;
+		/// <summary>
+		/// The minimum number of constraints to allocate space for in each individual type batch.
+		/// New type batches will be given enough memory for this number of constraints, and any compaction will not reduce the allocations below it.
+		/// The number of constraints can vary greatly across types- there are usually far more contacts than ragdoll constraints.
+		/// Per type estimates can be assigned within the Solver.TypeBatchAllocation if necessary. This value acts as a lower bound for all types.
+		/// </summary>
+		int32_t ConstraintsPerTypeBatch;
+		/// <summary>
+		/// The minimum number of constraints to allocate space for in each body's constraint list.
+		/// New bodies will be given enough memory for this number of constraints, and any compaction will not reduce the allocations below it.
+		/// </summary>
+		int32_t ConstraintCountPerBodyEstimate;
+
+		/// <summary>
+		/// Constructs a description of simulation allocations.
+		/// </summary>
+		/// <param name="bodies">The number of bodies to allocate space for.</param>
+		/// <param name="statics">The number of statics to allocate space for.</param>
+		/// <param name="islands">The number of inactive islands to allocate space for.</param>
+		/// <param name="shapesPerType">Minimum number of shapes to allocate space for in each shape type batch.</param>
+		/// <param name="constraints">The number of constraints to allocate bookkeeping space for. This does not affect actual type batch allocation sizes, only the solver-level constraint handle storage.</param>
+		/// <param name="constraintsPerTypeBatch">The minimum number of constraints to allocate space for in each individual type batch.
+		/// New type batches will be given enough memory for this number of constraints, and any compaction will not reduce the allocations below it.
+		/// The number of constraints can vary greatly across types- there are usually far more contacts than ragdoll constraints.
+		/// Per type estimates can be assigned within the Solver.TypeBatchAllocation if necessary. This value acts as a lower bound for all types.</param>
+		/// <param name="constraintCountPerBodyEstimate">The minimum number of constraints to allocate space for in each body's constraint list.
+		/// New bodies will be given enough memory for this number of constraints, and any compaction will not reduce the allocations below it.</param>
+		SimulationAllocationSizes(int32_t bodies = 4096, int32_t statics = 4096, int32_t islands = 16, int32_t shapesPerType = 128, int32_t constraints = 16384, int32_t constraintsPerTypeBatch = 256, int32_t constraintCountPerBodyEstimate = 8)
+		{
+			Bodies = bodies;
+			Statics = statics;
+			Islands = islands;
+			ShapesPerType = shapesPerType;
+			Constraints = constraints;
+			ConstraintsPerTypeBatch = constraintsPerTypeBatch;
+			ConstraintCountPerBodyEstimate = constraintCountPerBodyEstimate;
+		}
+	};
+
 	extern "C" void Initialize();
 	extern "C" void Destroy();
 	extern "C" SIMDWidth GetSIMDWidth();
