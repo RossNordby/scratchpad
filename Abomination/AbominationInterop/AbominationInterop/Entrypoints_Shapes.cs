4ffe42d82b5985248154715d5602ac42a385b112
Ross Nordby <norbo@bepuentertainment.com>
2022-08-03 14:08:45 -0500

    Shape removal, compound creation/destruction/inertia.

--- Abomination/AbominationInterop/AbominationInterop/Entrypoints_Shapes.cs
+++ Abomination/AbominationInterop/AbominationInterop/Entrypoints_Shapes.cs
@@ -1,5 +1,8 @@
 ﻿
+using BepuPhysics;
 using BepuPhysics.Collidables;
+using BepuUtilities.Memory;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -104,5 +107,90 @@ public static partial class Entrypoints
     public unsafe static TypedIndex AddMesh([TypeName(SimulationName)] InstanceHandle simulationHandle, Mesh mesh)
     {
         return simulations[simulationHandle].Shapes.Add(mesh);
+    }
+
+    /// <summary>
+    /// Removes a shape from the simulation. Does not return any shape allocated buffers to buffer pools.
+    /// </summary>
+    /// <param name="simulationHandle">Handle of the simulation to remove the shape from.</param>
+    /// <param name="shape">Shape to remove from the simulation.</param>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(RemoveShape))]
+    public unsafe static void RemoveShape([TypeName(SimulationName)] InstanceHandle simulationHandle, TypedIndex shape)
+    {
+        simulations[simulationHandle].Shapes.Remove(shape);
+    }
+
+    /// <summary>
+    /// Removes a shape from the simulation. If the shape has resources that were allocated from a buffer pool, they will be returned to the specified pool.
+    /// </summary>
+    /// <param name="simulationHandle">Handle of the simulation to remove the shape from.</param>
+    /// <param name="bufferPoolHandle">Buffer pool to return shape resources to, if any.</param>
+    /// <param name="shape">Shape to remove from the simulation.</param>
+    /// <remarks>The same buffer pool must be used for both allocation and deallocation.</remarks>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(RemoveShape))]
+    public unsafe static void RemoveAndDestroyShape([TypeName(SimulationName)] InstanceHandle simulationHandle, [TypeName(BufferPoolName)] InstanceHandle bufferPoolHandle, TypedIndex shape)
+    {
+        simulations[simulationHandle].Shapes.RemoveAndDispose(shape, bufferPools[bufferPoolHandle]);
+    }
+
+    /// <summary>
+    /// Removes a shape and all references child shapes from the simulation. If the shapes had resources that were allocated from a buffer pool, they will be returned to the specified pool.
+    /// </summary>
+    /// <param name="simulationHandle">Handle of the simulation to remove the shape from.</param>
+    /// <param name="bufferPoolHandle">Buffer pool to return shape resources to, if any.</param>
+    /// <param name="shape">Shape to remove from the simulation.</param>
+    /// <remarks>The same buffer pool must be used for both allocation and deallocation.</remarks>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(RemoveShape))]
+    public unsafe static void RemoveAndDestroyShapeRecursively([TypeName(SimulationName)] InstanceHandle simulationHandle, [TypeName(BufferPoolName)] InstanceHandle bufferPoolHandle, TypedIndex shape)
+    {
+        simulations[simulationHandle].Shapes.RecursivelyRemoveAndDispose(shape, bufferPools[bufferPoolHandle]);
+    }
+
+
+    /// <summary>
+    /// Creates a big compound shape from a list of children.
+    /// </summary>
+    /// <param name="simulationHandle">Handle of the simulation to which the shapes referenced by the compound children belong.</param>
+    /// <param name="bufferPoolHandle">Buffer pool to allocate resources from for the compound's acceleration structures.</param>
+    /// <param name="children">Children of the compound.</param>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(CreateBigCompound))]
+    public unsafe static BigCompound CreateBigCompound([TypeName(SimulationName)] InstanceHandle simulationHandle, [TypeName(BufferPoolName)] InstanceHandle bufferPoolHandle, Buffer<CompoundChild> children)
+    {
+        return new BigCompound(children, simulations[simulationHandle].Shapes, bufferPools[bufferPoolHandle]);
+    }
+    /// <summary>
+    /// Returns buffers allocated for a big compound shape.
+    /// </summary>
+    /// <param name="bufferPoolHandle">Buffer pool to return resources to. Must be the same pool that resources were allocated from.</param>
+    /// <param name="bigCompound">Big compound to destroy.</param>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(CreateBigCompound))]
+    public unsafe static void DestroyBigCompound([TypeName(BufferPoolName)] InstanceHandle bufferPoolHandle, BigCompound* bigCompound)
+    {
+        bigCompound->Dispose(bufferPools[bufferPoolHandle]);
+    }
+
+    /// <summary>
+    /// Computes the inertia associated with a set of compound children. Does not recenter the children.
+    /// </summary>
+    /// <param name="simulationHandle">Handle of the simulation to which the shapes referenced by the compound children belong.</param>
+    /// <param name="children">Children of the compound.</param>
+    /// <param name="childMasses">Masses of the children composing the compound.</param>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(ComputeCompoundInertiaWithoutRecentering))]
+    public unsafe static BodyInertia ComputeCompoundInertiaWithoutRecentering([TypeName(SimulationName)] InstanceHandle simulationHandle, Buffer<CompoundChild> children, Buffer<float> childMasses)
+    {
+        return CompoundBuilder.ComputeInertia(children, childMasses, simulations[simulationHandle].Shapes);
+    }
+
+    /// <summary>
+    /// Computes the inertia associated with a set of compound children. Recenters all children onto the computed local center of mass.
+    /// </summary>
+    /// <param name="simulationHandle">Handle of the simulation to which the shapes referenced by the compound children belong.</param>
+    /// <param name="children">Children of the compound.</param>
+    /// <param name="childMasses">Masses of the children composing the compound.</param>
+    /// <param name="centerOfMass">Computed center of mass that was subtracted from the position of compound children.</param>
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) }, EntryPoint = FunctionNamePrefix + nameof(ComputeCompoundInertia))]
+    public unsafe static BodyInertia ComputeCompoundInertia([TypeName(SimulationName)] InstanceHandle simulationHandle, Buffer<CompoundChild> children, Buffer<float> childMasses, Vector3* centerOfMass)
+    {
+        return CompoundBuilder.ComputeInertia(children, childMasses, simulations[simulationHandle].Shapes, out *centerOfMass);
     }
 }
