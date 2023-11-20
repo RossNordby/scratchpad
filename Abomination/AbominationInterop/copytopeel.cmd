a2ee509703a8a3faa361c84878bc58a94bcd417f
Ross Nordby <norbo@bepuentertainment.com>
2023-11-20 17:41:58 -0600

    Path oopsy fixes.

--- Abomination/AbominationInterop/copytopeel.cmd
+++ Abomination/AbominationInterop/copytopeel.cmd
@@ -1,5 +1,5 @@
 rem yes this is hardcoded but this is scratchpad. if you want to make this work...
 rem just put the peel repo in the parent directory of scratchpad repo
-robocopy AbominationInterop\bin\Release\net8.0\win-x64\native\ ..\..\..\peel\PEEL_PhysX_Edition\PEEL_Externals\Bepu\Release
-robocopy AbominationInterop\bin\Debug\net8.0\win-x64\native\ ..\..\..\peel\PEEL_PhysX_Edition\PEEL_Externals\Bepu\Debug
-robocopy BepuPhysicsCPP ..\..\..\peel\PEEL_PhysX_Edition\PEEL_Externals\Bepu *.h
+robocopy AbominationInterop\bin\Release\net8.0\win-x64\native\ ..\..\..\peel\PEEL_Externals\Bepu\Release
+robocopy AbominationInterop\bin\Debug\net8.0\win-x64\native\ ..\..\..\peel\PEEL_Externals\Bepu\Debug
+robocopy BepuPhysicsCPP ..\..\..\peel\PEEL_Externals\Bepu *.h
